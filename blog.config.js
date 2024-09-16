8036d0035f3b8124e9801496424d62c208bdfb7f
MENG-GUANLIN(Green) <88531334+paopaolin@users.noreply.github.com>
2024-09-16 09:48:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -292,7 +292,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || true, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
-  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
   MUSIC_PLAYER_VISIBLE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
   MUSIC_PLAYER_AUTO_PLAY:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
@@ -306,14 +306,14 @@ const BLOG = {
     {
       name: '明日への鼓動 ',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/outchain/player?type=2&id=1961981739.mp3',
+      url: 'https://music.163.com/outchain/player?type=2&id=1961981739',
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
     {
       name: '天気の子',
       artist: 'STRlighT',
-      url: 'https://music.163.com/outchain/player?type=2&id=1973129951.mp3',
+      url: 'https://music.163.com/outchain/player?type=2&id=1973129951',
       cover:
         'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     }
